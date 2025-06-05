3b256125a3709e875df59b317209d154fbcadcde
silaswpo <silaswesleypereiradeoliveira@gmail.com>
2025-06-04 22:38:26 -0300

    Validando

--- blink.v
+++ blink.v
@@ -1,33 +1,46 @@
 module Blink #(
-    parameter CLK_FREQ = 25_000_000  // Clock fixo da FPGA
+    parameter CLK_FREQ = 25_000_000
 ) (
     input wire clk,
     input wire rst_n,
     output reg [7:0] leds
 );
 
-localparam ON_TIME_CYCLES  = CLK_FREQ * 4; // 4 segundos
-localparam OFF_TIME_CYCLES = CLK_FREQ * 2; // 2 segundos
+localparam STEP_TIME = CLK_FREQ * 2;  // 2 segundos por etapa
 
 reg [31:0] counter;
-reg led_on;
+reg [3:0] index;  // contador de qual LED está sendo alterado
+reg ascending;    // 1 = ligando, 0 = desligando
 
 always @(posedge clk) begin
     if (!rst_n) begin
-        counter <= 32'd0;
-        led_on  <= 1'b1;
-        leds    <= 8'b01010101; // Liga LEDs 0,2,4,6
+        counter   <= 0;
+        index     <= 0;
+        ascending <= 1'b1;
+        leds      <= 8'b00000000;
     end else begin
         counter <= counter + 1;
 
-        if (led_on && counter >= ON_TIME_CYCLES) begin
+        if (counter >= STEP_TIME) begin
             counter <= 0;
-            led_on  <= 1'b0;
-            leds    <= 8'b11100000; // Desliga todos
-        end else if (!led_on && counter >= OFF_TIME_CYCLES) begin
-            counter <= 0;
-            led_on  <= 1'b1;
-            leds    <= 8'b01111111; // Liga 0,2,4,6
+
+            if (ascending) begin
+                leds[index] <= 1'b1;
+                if (index == 7) begin
+                    ascending <= 1'b0;
+                    index <= 0;
+                end else begin
+                    index <= index + 1;
+                end
+            end else begin
+                leds[index] <= 1'b0;
+                if (index == 7) begin
+                    ascending <= 1'b1;
+                    index <= 0;
+                end else begin
+                    index <= index + 1;
+                end
+            end
         end
     end
 end
